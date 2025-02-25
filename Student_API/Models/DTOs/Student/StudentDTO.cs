b2519850005f21484873b26edc3708931f5eae89
JohnDev <andreinicolas.codebank@gmail.com>
2025-02-25 21:14:10 +0800

    Fix the Type in the entiry and in the DTo

--- Student_API/Models/DTOs/Student/StudentDTO.cs
+++ Student_API/Models/DTOs/Student/StudentDTO.cs
@@ -6,12 +6,12 @@ namespace Student_API.Models.DTOs.Student
     {
         public int StudentId { get; set; }
         public required string Name { get; set; } 
-        public required DateOnly? Birthday { get; set; }
-        public string Email { get; set; }
+        public required DateOnly Birthday { get; set; }
+        public required string Email { get; set; }
         public required string PhoneNumber { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
         public AddressDTO? Address { get; set; } 
     }
 }   
