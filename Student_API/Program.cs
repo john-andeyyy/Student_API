a0cf40cb20f99a972735c96c655182ac609009db
JohnDev <andreinicolas.codebank@gmail.com>
2025-02-28 22:03:19 +0800

    add a appsettings Prod and add a entry message for testing

--- Student_API/Program.cs
+++ Student_API/Program.cs
@@ -9,11 +9,36 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+// the if else statement is used to check if the environment is development or production
+var env = builder.Environment;
+
+string connectionString;
+
+if(env.IsDevelopment())
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else if (env.IsProduction())
+{
+    connectionString = builder.Configuration.GetConnectionString("ProductionConnection");
+}
+else
+{
+    throw new Exception("Invalid Environment Configuration!");
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")) 
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
     ));
+
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseMySql(
+//        builder.Configuration.GetConnectionString("DefaultConnection"),
+//        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")) 
+//));
 // add-migration "Initial Migration"
 // to run the migration --- update-database
 
@@ -32,5 +57,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGet("/", () => Results.Json(new { message = "hi" }));
 
 app.Run();
