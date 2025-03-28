7338c9d70bd7b38e29c576e562843f27ab44f82e
JohnDev <andreinicolas.codebank@gmail.com>
2025-03-28 23:43:22 +0800

    using &&

--- Student_API/Program.cs
+++ Student_API/Program.cs
@@ -86,6 +86,6 @@ app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapGet("/", () => Results.Json(new { message = "Hello world!!, using git action test 4" }));
+app.MapGet("/", () => Results.Json(new { message = "Hello world!!, using git action test 5" }));
 
 app.Run();
